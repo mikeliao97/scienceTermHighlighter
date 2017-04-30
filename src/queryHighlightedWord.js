ea06c87e15e0311ab7747bbe0102c24e73233995
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-04-29 18:08:43 -0700

    add event listener to get highlighted word on double click

--- src/queryHighlightedWord.js
+++ src/queryHighlightedWord.js
@@ -1,17 +1,16 @@
-// Called when the user clicks on the chrome extension.
-chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript({
-    code: JSON.stringify(getHighlightedWord)
-  });
-});
-
-const getHighlightedWord = (() => {
+const getHighlightedWord = () => {
   let text = '';
   if (window.getSelection) {
     text = window.getSelection().toString();
   } else if (document.selection && document.selection.type != "Control") {
     text = document.selection.createRange().text;
   }
-  console.log(text);
   return text;
-})();
+};
+
+const queryHighlightedWord = () => {
+  const highlightedWord = getHighlightedWord();
+  console.log('highlighted word:', highlightedWord);
+};
+
+document.addEventListener('dblclick', queryHighlightedWord);
